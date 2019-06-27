222a1a3be165b37635fc847e4a6b9bed142227c9
gameu3d <sunstar198941@gmail.com>
2019-06-27 09:31:04 +0800

    tutorial of mind your step

---
+++ assets/Scipts/GameManager.ts
@@ -0,0 +1,121 @@
+import { _decorator, Component, Prefab, instantiate, Node, LabelComponent, CCInteger} from "Cocos3D";
+import { PlayerController } from "./PlayerController";
+const { ccclass, property } = _decorator;
+
+enum BlockType{
+    BT_NONE,
+    BT_STONE,
+};
+
+enum GameState{
+    GS_INIT,
+    GS_PLAYING,
+    GS_END,
+};
+
+@ccclass("GameManager")
+export class GameManager extends Component {
+
+    @property({type: Prefab})
+    public cubePrfb: Prefab = null;
+    @property({type: CCInteger})
+    public roadLength: Number = 50;
+    private _road: number[] = [];
+    @property({type: Node})
+    public startMenu: Node = null;
+    @property({type: PlayerController})
+    public playerCtrl: PlayerController = null;
+    private _curState: GameState = GameState.GS_INIT;
+    @property({type: LabelComponent})
+    public stepsLabel: LabelComponent = null;
+
+    start () {
+        this.curState = GameState.GS_INIT;
+        this.playerCtrl.node.on('JumpEnd', this.onPlayerJumpEnd, this);
+    }
+
+    init() {
+        this.startMenu.active = true;
+        this.generateRoad();
+        this.playerCtrl.setInputActive(false);
+        this.playerCtrl.node.setPosition(cc.v3());
+        this.playerCtrl.reset();
+    }
+
+    set curState (value: GameState) {
+        switch(value) {
+            case GameState.GS_INIT:
+                this.init();
+                break;
+            case GameState.GS_PLAYING:
+                this.startMenu.active = false;
+                this.stepsLabel.string = '0';   // 将步数重置为0
+                setTimeout(() => {      //直接设置active会直接开始监听鼠标事件，做了一下延迟处理
+                    this.playerCtrl.setInputActive(true);
+                }, 0.1);
+                break;
+            case GameState.GS_END:
+                break;
+        }
+        this._curState = value;
+    }
+
+    generateRoad() {
+
+        this.node.removeAllChildren(true);
+
+        this._road = [];
+        // startPos
+        this._road.push(BlockType.BT_STONE);
+
+        for (let i = 1; i < this.roadLength; i++) {
+            if (this._road[i-1] === BlockType.BT_NONE) {
+                this._road.push(BlockType.BT_STONE);
+            } else {
+                this._road.push(Math.floor(Math.random() * 2));
+            }
+        }
+
+        for (let j = 0; j < this._road.length; j++) {
+            let block: Node = this.spawnBlockByType(this._road[j]);
+            if (block) {
+                this.node.addChild(block);
+                block.setPosition(j, -1.5, 0);
+            }
+        }
+    }
+
+    spawnBlockByType(type: BlockType) {
+        let block = null;
+        switch(type) {
+            case BlockType.BT_STONE:
+                block = instantiate(this.cubePrfb);
+                break;
+        }
+
+        return block;
+    }
+
+    onStartButtonClicked() {
+        this.curState = GameState.GS_PLAYING;
+    }
+
+    checkResult(moveIndex: number) {
+        if (moveIndex <= this.roadLength) {
+            if (this._road[moveIndex] == BlockType.BT_NONE) {   //跳到了空方块上
+                this.curState = GameState.GS_INIT;
+            }
+        } else {    // 跳过了最大长度
+            this.curState = GameState.GS_INIT;
+        }
+    }
+
+    onPlayerJumpEnd(moveIndex: number) {
+        this.stepsLabel.string = '' + moveIndex;
+        this.checkResult(moveIndex);
+    }
+
+    // update (deltaTime: number) {
+    //     // Your update function goes here.
+    // }
+}
---
+++ assets/Scipts/PlayerController.ts
@@ -0,0 +1,90 @@
+import { _decorator, Component, Vec3, vmath, systemEvent, SystemEvent, EventMouse, AnimationComponent } from "Cocos3D";
+const { ccclass, property } = _decorator;
+
+@ccclass("PlayerController")
+export class PlayerController extends Component {
+
+    @property({type: AnimationComponent})
+    public BodyAnim: AnimationComponent = null;
+
+    // for fake tween
+    private _startJump: boolean = false;
+    private _jumpStep: number = 0;
+    private _curJumpTime: number = 0;
+    private _jumpTime: number = 0.1;
+    private _curJumpSpeed: number = 0;
+    private _curPos: Vec3 = cc.v3();
+    private _deltaPos: Vec3 = cc.v3(0, 0, 0);
+    private _targetPos: Vec3 = cc.v3();
+    private _isMoving = false;
+    private _curMoveIndex = 0;
+
+    start () {
+    }
+
+    reset() {
+        this._curMoveIndex = 0;
+    }
+
+    setInputActive(active: boolean) {
+        if (active) {
+            systemEvent.on(SystemEvent.EventType.MOUSE_UP, this.onMouseUp, this);
+        } else {
+            systemEvent.off(SystemEvent.EventType.MOUSE_UP, this.onMouseUp, this);
+        }
+    }
+
+    onMouseUp(event: EventMouse) {
+        if (event.getButton() === 0) {
+            this.jumpByStep(1);
+        } else if (event.getButton() === 2) {
+            this.jumpByStep(2);
+        }
+
+    }
+
+    jumpByStep(step: number) {
+        if (this._isMoving) {
+            return;
+        }
+        this._startJump = true;
+        this._jumpStep = step;
+        this._curJumpTime = 0;
+        this._curJumpSpeed = this._jumpStep / this._jumpTime;
+        this.node.getPosition(this._curPos);
+        vmath.vec3.add(this._targetPos, this._curPos, cc.v3(this._jumpStep, 0, 0));
+
+        this._isMoving = true;
+
+        if (step === 1) {
+            this.BodyAnim.play('oneStep');
+        } else if (step === 2) {
+            this.BodyAnim.play('twoStep');
+        }
+
+        this._curMoveIndex += step;
+    }
+
+    onOnceJumpEnd() {
+        this._isMoving = false;
+        this.node.emit('JumpEnd', this._curMoveIndex);
+    }
+
+    update (deltaTime: number) {
+        if (this._startJump) {
+            this._curJumpTime += deltaTime;
+            if (this._curJumpTime > this._jumpTime) {
+                // end
+                this.node.setPosition(this._targetPos);
+                this._startJump = false;
+                this.onOnceJumpEnd();
+            } else {
+                // tween
+                this.node.getPosition(this._curPos);
+                this._deltaPos.x = this._curJumpSpeed * deltaTime;
+                vmath.vec3.add(this._curPos, this._curPos, this._deltaPos);
+                this.node.setPosition(this._curPos);
+            }
+        }
+    }
+}
